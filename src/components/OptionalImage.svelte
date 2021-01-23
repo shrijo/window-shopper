fbb09d5ff1369e30d590af41d6bbf5bd0c5e1e56
Alex Jenter <jenteralex@gmail.com>
2021-01-23 14:58:49 +0100

    ensure image aspect ratio

--- src/components/OptionalImage.svelte
+++ src/components/OptionalImage.svelte
@@ -5,11 +5,17 @@
 {#if image}
   <img src="{image}" alt="d" />
 {:else}
-  <img src="https://via.placeholder.com/150/0000FF/FFFFFF" alt="" /> 
+  <img
+    width="1"
+    height="1"
+    src="https://via.placeholder.com/150/0000FF/FFFFFF"
+    alt=""
+  /> 
 {/if}
 
 <style>
   img {
-    width: 100%;
+    width:100%;
+    height:auto;
   }
 </style>

4fb529e296d5b1840c7f716e097a81037cb43785
Alex Jenter <jenteralex@gmail.com>
2021-01-24 14:13:22 +0100

    add cliche placeholder image

--- src/components/OptionalImage.svelte
+++ src/components/OptionalImage.svelte
@@ -2,20 +2,24 @@
   export let image = false;
 </script>
 
-{#if image}
-  <img src="{image}" alt="d" />
-{:else}
-  <img
-    width="1"
-    height="1"
-    src="https://via.placeholder.com/150/0000FF/FFFFFF"
-    alt=""
-  /> 
-{/if}
-
 <style>
   img {
     width:100%;
     height:auto;
   }
 </style>
+
+{#if image}
+  <img src="{image}" alt="d" />
+{:else}
+  <svg viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
+    <g fill="none" stroke="black" stroke-width="0.01">
+      <path d="M0,0 L1,1 M0,1 L1,0" />
+      <rect stroke-width="0.02"
+        x="0"
+        y="0"
+        height="1"
+        width="1" />
+    </g>
+  </svg>
+{/if}

be1b8a1f5fd9934341e6c58542cded49d1eb6b57
Alex Jenter <jenteralex@gmail.com>
2021-01-31 14:53:23 +0100

    adds drop shadow

--- src/components/A4.svelte
+++ src/components/A4.svelte
@@ -36,9 +36,9 @@
     display: grid;
     width: 100%;
     background-color: #ffffff;
-    /* -webkit-box-shadow: 0px 10px 53px -23px rgba(0, 0, 0, 0.52);
+    -webkit-box-shadow: 0px 10px 53px -23px rgba(0, 0, 0, 0.52);
     -moz-box-shadow: 0px 10px 53px -23px rgba(0, 0, 0, 0.52);
-    box-shadow: 0px 10px 53px -23px rgba(0, 0, 0, 0.52); */
+    box-shadow: 0px 10px 53px -23px rgba(0, 0, 0, 0.52);
   }
   .A4.fitHeight {
     width: var(--w);

219d00b75bc70ce08ef3af7146d78fd61187c60d
Alex Jenter <jenteralex@gmail.com>
2021-01-31 14:46:11 +0100

    aspect ratio

--- src/App.svelte
+++ src/App.svelte
@@ -1,140 +1,148 @@
 <script>
-	import marked from "marked";
-	import A4 from "./components/A4.svelte";
-	import Debug from "./components/debug.svelte";
-	import ImageUpload from "./components/form/ImageUpload.svelte";
-	import LabeledInput from "./components/form/LabeledInput.svelte";
-	import LabeledTextarea from "./components/form/LabeledTextarea.svelte";
-	import OptionalImage from "./components/OptionalImage.svelte";
-	import QRCode from "./components/QRCode.svelte";
-	import QRCodeSection from "./components/form/QRCodeSection.svelte";
+  import marked from "marked";
+  import A4 from "./components/A4.svelte";
+  import Debug from "./components/debug.svelte";
+  import ImageUpload from "./components/form/ImageUpload.svelte";
+  import LabeledInput from "./components/form/LabeledInput.svelte";
+  import LabeledTextarea from "./components/form/LabeledTextarea.svelte";
+  import OptionalImage from "./components/OptionalImage.svelte";
+  import QRCode from "./components/QRCode.svelte";
+  import QRCodeSection from "./components/form/QRCodeSection.svelte";
 
-	import { link, title, subtitle, text } from "./stores";
-	let image = false;
+  import { link, title, subtitle, text } from "./stores";
+  let image = false;
 
-	const onPrint = () => {
-		window.print()
-	};
+  const onPrint = () => {
+    window.print();
+  };
 </script>
 
-
 <main>
-	<div id="editor">
-		<h1>Window Shopper</h1>
-		<form>
-			<LabeledInput label="Titel" placeholder="Produkt Titel" bind:value={$title} />
-			<LabeledInput label="Untertitel" placeholder="Produkt Preis" bind:value={$subtitle} />
-			<LabeledTextarea label="Text" placeholder="Produkt beschreiben" bind:value={$text} />
-			<ImageUpload bind:value={image}/>
-			<QRCodeSection />
-			<button on:click={onPrint} type="button" class="print-button">Print!</button>
-		</form>
+  <div id="editor">
+    <h1>Window Shopper</h1>
+    <form>
+      <LabeledInput
+        label="Titel"
+        placeholder="Produkt Titel"
+        bind:value={$title}
+      />
+      <LabeledInput
+        label="Untertitel"
+        placeholder="Produkt Preis"
+        bind:value={$subtitle}
+      />
+      <LabeledTextarea
+        label="Text"
+        placeholder="Produkt beschreiben"
+        bind:value={$text}
+      />
+      <ImageUpload bind:value={image} />
+      <QRCodeSection />
+      <button on:click={onPrint} type="button" class="print-button"
+        >Print!</button
+      >
+    </form>
 
-		<Debug vars={{
-      link: $link,
-      subtitle: $subtitle,
-      title: $title
-    }} />
-
-	</div>
-	<div id="preview">
-		<A4>
-			<OptionalImage bind:image/>
-			<section>
-				<div class="text">
-					<h2>{$title}</h2>
-					<h4>{$subtitle}</h4>
-					<p class="description">{@html marked($text)}</p>
-				</div>
-				<QRCode />
-			</section>
-		</A4>
-	</div>
-
+    <Debug
+      vars={{
+        link: $link,
+        subtitle: $subtitle,
+        title: $title,
+      }}
+    />
+  </div>
+  <div id="preview">
+    <A4>
+      <OptionalImage bind:image />
+      <section>
+        <div class="text">
+          <h2>{$title}</h2>
+          <h4>{$subtitle}</h4>
+          <p class="description">{@html marked($text)}</p>
+        </div>
+        <QRCode />
+      </section>
+    </A4>
+  </div>
 </main>
 
-
 <style>
-	main {
-		width: 100vw;
-		height: 100vh;
-		text-align: left;
-		margin: 0;
-		padding: 0;
-		display: flex;
-		margin: 0 auto;
+  main {
+    width: 100vw;
+    height: 100vh;
+    text-align: left;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    margin: 0 auto;
 
-		box-sizing: border-box;
-	}
+    box-sizing: border-box;
+  }
 
-	:global(label){
-		font-size: 0.8em;
-		color: #a1a1a1;
-		margin-bottom: 3px;
-	}
+  :global(label) {
+    font-size: 0.8em;
+    color: #a1a1a1;
+    margin-bottom: 3px;
+  }
 
-	:global(input),
-	:global(textarea) {
-		width: 100%;
-		background-color: #efefef;
-		border: none;
-	}
-	form{
-		border-top: 1px solid #000000;
-		padding: 10px;
-	}
+  :global(input),
+  :global(textarea) {
+    width: 100%;
+    background-color: #efefef;
+    border: none;
+  }
+  form {
+    border-top: 1px solid #000000;
+    padding: 10px;
+  }
 
-	#editor {
-		margin: 10px;
-		border: 1px solid #000000;
-		z-index: 10;
-		background: white;
-		box-sizing: border-box;
-		/* width: 30vw; */
-		width: 400px;
-		flex-shrink: 0;
-		position: sticky;
-		top: 0;
-	}
+  #editor {
+    margin: 10px;
+    border: 1px solid #000000;
+    z-index: 10;
+    background: white;
+    box-sizing: border-box;
+    /* width: 30vw; */
+    width: 400px;
+    flex-shrink: 0;
+    position: sticky;
+    top: 0;
+  }
 
-	.print-button{
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		width: 100%;
-		background-color: #000000;
-		border: none;
-		color: #ffffff;
-		font-weight: 600;
-		line-height: 2em;
-	}
+  .print-button {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: #000000;
+    border: none;
+    color: #ffffff;
+    font-weight: 600;
+    line-height: 2em;
+  }
 
-	#preview {
-		flex-grow: 1;
-		flex-shrink: 1;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-	h1 {
-		padding: 0.1em 10px 0 10px;
-		line-height: 2em;
-		margin: 0;
-		color: #000000;
-		text-transform: uppercase;
-		font-size: 1.3em;
-		font-weight: 1000;
-	}
-	section {
-		display: flex;
-		margin-top: min(auto, 8mm);
-	}
-	.text {
-		flex-grow: 1;
-		margin-right: 2rem;
-	}
-	.description {
-		text-align: justify;
-	}
-
+  #preview {
+    flex-grow: 1;
+    flex-shrink: 1;
+  }
+  h1 {
+    padding: 0.1em 10px 0 10px;
+    line-height: 2em;
+    margin: 0;
+    color: #000000;
+    text-transform: uppercase;
+    font-size: 1.3em;
+    font-weight: 1000;
+  }
+  section {
+    display: flex;
+    margin-top: min(auto, 8mm);
+  }
+  .text {
+    flex-grow: 1;
+    margin-right: 2rem;
+  }
+  .description {
+    text-align: justify;
+  }
 </style>
--- src/components/A4.svelte
+++ src/components/A4.svelte
@@ -1,23 +1,52 @@
+<script>
+  let w, h;
+  let pageRatio = 210 / 297;
+  let viewRatio = w / h;
+  let fitHeight = pageRatio < viewRatio;
+  $: {
+    viewRatio = w / h;
+    fitHeight = pageRatio < viewRatio;
+    console.log({ w, h, fitHeight });
+  }
+</script>
+
+<div
+  class="container"
+  bind:clientWidth={w}
+  bind:clientHeight={h}
+  style="--w: {0.707 * (h - 20)}px; --h: {h - 20}px"
+>
+  <div class="A4" class:fitHeight>
+    <svg viewBox="0 0 210 297">
+      <path stroke="red" d="M0,0 L210,297" />
+    </svg>
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+</div>
+
 <style>
+  .container {
+    margin: 10px;
+    margin-left: 0;
+    height: 100%;
+  }
   .A4 {
-    box-sizing: border-box; 
+    display: grid;
+    width: 100%;
+    background-color: #ffffff;
+    /* -webkit-box-shadow: 0px 10px 53px -23px rgba(0, 0, 0, 0.52);
+    -moz-box-shadow: 0px 10px 53px -23px rgba(0, 0, 0, 0.52);
+    box-shadow: 0px 10px 53px -23px rgba(0, 0, 0, 0.52); */
+  }
+  .A4.fitHeight {
+    width: var(--w);
+    height: var(--h);
+  }
 
-
-   width: 70vmin;
-   height: 100vmin;
-   max-width: 100%;
-   max-height: 100%;
-   object-fit: contain;
-
-    
-
-		background-color: #ffffff;
-		-webkit-box-shadow: 0px 10px 53px -23px rgba(0,0,0,0.52);
-		-moz-box-shadow: 0px 10px 53px -23px rgba(0,0,0,0.52);
-		box-shadow: 0px 10px 53px -23px rgba(0,0,0,0.52);
+  .A4 svg,
+  .A4 .content {
+    grid-area: 1 / 1 / 2 / 2;
   }
 </style>
-
-<div class="A4">
-  <slot></slot>
-</div>
